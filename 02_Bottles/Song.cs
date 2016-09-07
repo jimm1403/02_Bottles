5438366102dce2860e734d0e1304a9c9f6a8a412
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-07 19:24:30 +0200

    Added song class, and CountBottles method

---
+++ 02_Bottles/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02_Bottles
+{
+    internal class Song
+    {
+
+        internal string CountBottles(int _bottles)
+        {
+            for (_bottles = 3; _bottles > 0; _bottles--)
+            {
+                if (_bottles > 1)
+                {
+
+                }
+            }
+            
+        }
+
+    }
+    }
+}
