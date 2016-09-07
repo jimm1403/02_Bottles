798f2e09f88dbfd98e071fa6f34a0e95af1a2141
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-07 20:22:57 +0200

    Added Assert.AreEqual() test to all testmethods. Added loop to check for bottleCount

--- 02_Bottles/Song.cs
+++ 02_Bottles/Song.cs
@@ -4,19 +4,50 @@ namespace _02_Bottles
 {
     internal class Song
     {
+        string _zero = " No more bottles of beer on the wall.";
+        string _one = "1 bottle of beer on the wall."
+                    + " 1 bottle of beer."
+                    + " Take one down and pass it around."
+                    + " No more bottles of beer on the wall.";
+        string _two = "2 bottles of beer on the wall."
+                    + " 2 bottles of beer."
+                    + " Take one down and pass it around."
+                    + " 1 bottle of beer on the wall."
+                    + " 1 bottle of beer on the wall."
+                    + " 1 bottle of beer."
+                    + " Take one down and pass it around."
+                    + " No more bottles of beer on the wall."; 
+        string _three = "3 bottles of beer on the wall."
+                    + " 3 bottles of beer."
+                    + " Take one down and pass it around."
+                    + " 2 bottles of beer on the wall."
+                    + " 2 bottles of beer on the wall."
+                    + " 2 bottles of beer."
+                    + " Take one down and pass it around."
+                    + " 1 bottle of beer on the wall."
+                    + " 1 bottle of beer on the wall."
+                    + " 1 bottle of beer."
+                    + " Take one down and pass it around."
+                    + " No more bottles of beer on the wall.";
 
-        internal string CountBottles(int _bottles)
+        internal object CountBottles(int bottles)
         {
-            for (_bottles = 3; _bottles > 0; _bottles--)
+            string line = "";
+            
+            for (int _bottles = 3; _bottles > 0 ; _bottles--)
             {
-                if (_bottles > 1)
+                if (bottles == 3)
+                    line = _three;
+                else if (bottles == 2)
+                    line = _two;
+                else if (bottles == 1)
+                    line = _one;
+                else
                 {
-
+                    line = _zero;
                 }
             }
-            
+                return line;
         }
-
-    }
     }
 }
